a7f26caf0913c0c6323919fe1e6c34398b178fc4
Miotto99 <140741487+Miotto99@users.noreply.github.com>
2023-09-17 00:13:11 -0300

    Update M_Resultado.java

--- src/main/java/com/example/sorteador/Model/M_Resultado.java
+++ src/main/java/com/example/sorteador/Model/M_Resultado.java
@@ -1,19 +1,46 @@
-package com.example.sorteador.Model;
+package com.example.demo.Model;
 
-public class M_Resultado {
-    private int qtdNum;
+public class M_Sorteio {
+    private int quantosNumeros;
+    private int numeroMaximo;
+    private int numeroMinimo;
     private int[] resultados;
 
-    public M_Resultado(int qtdNum, int[] resultados) {
-        this.qtdNum = qtdNum;
+    public M_Sorteio(int quantosNumeros, int[] resultados, int numeroMinimo, int numeroMaximo) {
+        this.quantosNumeros = quantosNumeros;
         this.resultados = resultados;
+        this.numeroMinimo = numeroMinimo;
+        this.numeroMaximo = numeroMaximo;
     }
 
-    public int getQtdNum() {
-        return qtdNum;
+    public int getQuantosNumeros() {
+        return quantosNumeros;
+    }
+
+    public void setQuantosNumeros(int quantosNumeros) {
+        this.quantosNumeros = quantosNumeros;
+    }
+
+    public int getNumeroMaximo() {
+        return numeroMaximo;
+    }
+
+    public void setNumeroMaximo(int numeroMaximo) {
+        this.numeroMaximo = numeroMaximo;
+    }
+
+    public int getNumeroMinimo() {
+        return numeroMinimo;
+    }
+
+    public void setNumeroMinimo(int numeroMinimo) {
+        this.numeroMinimo = numeroMinimo;
     }
 
     public int[] getResultados() {
         return resultados;
+    }
+
+    public void setResultados(int[] resultados) {
     }
 }
